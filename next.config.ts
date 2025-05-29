57002faf7e3f63d3361e85583e94264561919d5c
Felix Reiterer <reitererfle>
2025-05-29 22:16:25 +0200

    CORS Adaption

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,10 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
-  /* config options here */
   reactStrictMode: true,
-};
+  images: {
+    domains: ['cdn.sanity.io'],
+  },
+}
 
-export default nextConfig;
+export default nextConfig
